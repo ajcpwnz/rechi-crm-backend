0a589a6c846bd1508f0833dc96852b3c804964fe
Alex <ajcpwnz@d3feat.website>
2023-08-19 21:13:09 +0300

    feat: list form submissions

---
+++ utils/pagination.ts
@@ -0,0 +1,16 @@
+import FormSubmission from '../models/FormSubmission'
+
+export const getPagination = (page: number, size: number) => {
+  const limit = size ? +size : 3;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+export const getPagingData = (data: {rows: FormSubmission[], count: number}, page: number, limit: number) => {
+  const { count: totalItems, rows } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, rows, totalPages, currentPage };
+};
